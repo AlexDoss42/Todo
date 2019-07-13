00c4c880c1b643417b7516dbd33089b6e553d713
@AlexDoss42 <alex.doss42@gmail.com>
2019-07-12 20:02:09 -0600

    Builds out basic structure of app

--- App.js
+++ App.js
@@ -1,19 +1,60 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+export default class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      todosList: []
+    }
+  }
+
+  render() {
+
+    return (
+      <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.headerText}>Doss's Dope ToDos</Text>
+      </View>
+      <View style={styles.inputContainer}>
+        <TextInput placeholder="add todo here" style={{color: 'red', textDecorationLine: 'underline'}}></TextInput>
+        <Button title="add to list" color='blue'></Button>
+      </View>
+      <View style={styles.listContainer}>
+        <Text>Get this Bread</Text>
+        <Text>Obtain this Grain</Text>
+        <Text>Entrust this Crust</Text>
+        <Text>YEET THIS WHEAT</Text>
+      </View>
     </View>
-  );
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  header: {
+    flex: 1,
+    marginTop: 50
+  },
+  headerText: {
+    color: 'blue',
+    fontSize: 32,
+    textTransform: 'uppercase',
+    fontWeight: 'bold'
+  },
+  inputContainer: {
+    flex: 1,
+    backgroundColor: "white"
+  },
+  listContainer: {
+    flex: 3,
+    backgroundColor: 'red'  
+  }
 });
